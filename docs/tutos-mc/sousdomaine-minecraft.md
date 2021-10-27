98b9770b698e6505135ebfd1ca8b898cb96a91b3
shotodev <shotoleftthegame@gmail.com>
2021-10-27 15:42:01 +0200

    Ajout des fichiers
    v1.0.0

---
+++ docs/tutos-mc/sousdomaine-minecraft.md
@@ -0,0 +1,39 @@
+---
+sidebar_position: 3
+---
+
+# Mettre un sous-domaine
+
+Bonjour à vous et bienvenue sur ce tuto simple et rapide pour mettre un sous domaine style **play.monserveur.com** sur votre serveur Minecraft.
+
+### Prérequis
+
+> - Un nom de domaine (Ici, on l'a mis sur Cloudflare, mais ca marche avec les autres sites sans problème)
+> - Un serveur de jeux (Chez [ExoHost](https://exohost.fr/store/minecraft) par exemple)
+
+*Dans cet exemple, je possède le domaine `dek.am` et je veux me connecter avec `play.dek.am` sur mon serveur `srv01.exohost.fr:25987`.*
+
+### Redirection CNAME
+
+D'abord, faites une redirection **CNAME** sur votre adresse de connexion de base (dans mon exemple, c'est `srv01.exohost.fr:25987`) comme indiqué ci-dessous :
+
+> ![Mettre un sous domaine - #1](https://i.imgur.com/8UxUIYV.png)
+
+> - Type : **CNAME**
+> - Nom : ce que vous voulez (dans mon cas, j'ai mis ``*play*`` pour avoir play.dek.am)
+> - Cible : votre adresse de connexion de base (dans mon cas, j'ai mis ``srv01.exohost.fr:25987``)
+
+### Redirection SRV
+
+Ensuite, faite une autre redirection **SRV** pour pouvoir vous connecter sans mettre de port à votre adresse de connexion comme ceci :
+`
+
+> ![Mettre un sous domaine - #2](https://i.imgur.com/NbEfNg9.png)
+> - Type : **SRV**
+> - Nom : ce que vous voulez (dans mon cas, j'ai mis ``*play*`` pour avoir play.dek.am)
+> - Service : **_minecraft**
+> - Protocole : **TCP**
+> - Priorité : **3600**
+> - Poids : **5**
+> - Port : Le port de votre serveur
+> - Cible : Ce que vous avez mis dans l'étape précecdente
---
+++ docusaurus.config.js
@@ -0,0 +1,118 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Documentation ExoHost',
+  tagline: 'Bienvenue sur la doc ExoHost. Ici, vous trouverez tout ce que vous aurez besoin.',
+  url: 'https://exohost.fr/',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'https://exohost.fr/assets/img/icon.png',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+  i18n: {
+    defaultLocale: 'fr',
+    locales: ['fr', 'en'],
+  },
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+        },
+        blog: {
+          showReadingTime: false,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'ExoHost',
+        logo: {
+          alt: 'ExoHost - Hébergeur Minecraft, GTA RP, GMOD, ...',
+          src: 'https://exohost.fr/assets/img/icon.png',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://exohost.fr',
+            label: 'Site ExoHost',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Contact',
+            items: [
+              {
+                label: 'Discord',
+                href: 'https://discord.exohost.fr',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/exohostfr',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} ExoHost. Tous droits réservés.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
