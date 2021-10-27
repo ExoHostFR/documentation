df5f8926d1ef30e8de22c63dbf11729dfe3ddd53
Sayrix <43046854+Sayrix@users.noreply.github.com>
2021-10-27 16:23:56 +0200

    Suppresion du blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -59,7 +59,6 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://exohost.fr',
             label: 'Site ExoHost',
@@ -95,10 +94,6 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',
